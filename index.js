6477b9544bb861218c65c7cb2c655c434b367c0d
Yiyi Wang <yiyi.wang@runtimeverification.com>
2023-04-10 19:22:08 +0800

    feat: Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+module.exports = {
+  // eslint-configs
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "plugin:eslint-comments/recommended",
+    "plugin:unicorn/recommended",
+  ],
+  // eslint-plugins
+  plugins: ["@typescript-eslint/eslint-plugin"],
+  // enabling/disabling/changing level of rules
+  rules: {
+    "@typescript-eslint/interface-name-prefix": "off",
+    "unicorn/no-null": "off",
+    "unicorn/prefer-module": "off",
+    "unicorn/prefer-top-level-await": "off",
+    "unicorn/prevent-abbreviations": [
+      "error",
+      {
+        replacements: {
+          e: {
+            event: false,
+          },
+          res: false,
+          req: false,
+          obj: false,
+          env: false,
+          cmd: {
+            command: true,
+          },
+        },
+      },
+    ],
+    quotes: ["error", "single", { avoidEscape: true }],
+  },
+};
