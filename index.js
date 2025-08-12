eab88164df8bd112fcf9f6b5826dff030bf3b720
asavienko <asavienko@gmail.com>
2025-08-12 12:03:45 +0200

    refactor: Migrate index.js to ESM and update ESLint configuration

--- index.js
+++ index.js
@@ -1,37 +1,28 @@
-module.exports = {
-  // eslint-configs
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-    'plugin:eslint-comments/recommended',
-    'plugin:unicorn/recommended',
-  ],
-  // eslint-plugins
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  // enabling/disabling/changing level of rules
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/prefer-nullish-coalescing': 'error',
-    'unicorn/no-null': 'off',
-    'unicorn/prefer-module': 'off',
-    'unicorn/prefer-top-level-await': 'off',
-    'unicorn/prevent-abbreviations': [
-      'error',
-      {
-        replacements: {
-          e: {
-            event: false,
-          },
-          res: false,
-          req: false,
-          obj: false,
-          env: false,
-          cmd: {
-            command: true,
-          },
-        },
-      },
-    ],
-    quotes: ['error', 'single', { avoidEscape: true }],
+// index.js (ESM)
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import unicorn from 'eslint-plugin-unicorn';
+import eslintComments from '@eslint-community/eslint-plugin-eslint-comments';
+import prettierRecommended from 'eslint-plugin-prettier/recommended'; // flat-config export
+import prettierCompat from 'eslint-config-prettier/flat';
+
+export default tseslint.config(
+  js.configs.recommended,
+  tseslint.configs.recommended,
+  unicorn.configs.recommended,                                   // plugin flat preset
+  { plugins: { 'eslint-comments': eslintComments },
+    rules: eslintComments.configs?.recommended?.rules ?? {} },    // apply recommended rules
+  prettierRecommended,                                            // enable prettier rule + set
+  prettierCompat,                                                 // turn off conflicting rules
+  {
+    rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/prefer-nullish-coalescing': 'error',
+      'unicorn/no-null': 'off',
+      'unicorn/prefer-module': 'off',
+      'unicorn/prefer-top-level-await': 'off',
+      'unicorn/prevent-abbreviations': ['error', { replacements: { e: { event: false }, res: false, req: false, obj: false, env: false, cmd: { command: true } } }],
+      quotes: ['error', 'single', { avoidEscape: true }],
+    },
   },
-};
+);
